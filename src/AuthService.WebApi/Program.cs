ea055f67886bece145fb99d7b33cb5f58440e89f
Shadrin Maksim <shadrin.maksim.sm@gmail.com>
2025-08-09 17:28:25 +0300

    chore(authservice): создать базовую структуру сервиса

---
+++ src/AuthService.WebApi/Program.cs
@@ -0,0 +1,27 @@
+namespace AuthService.WebApi;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        var app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+            app.UseSwaggerUI(opts =>
+            {
+                opts.SwaggerEndpoint("/openapi/v1.json", "AuthService API");
+            });
+        }
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
---
+++ tools/AuthService.MigrationsJob/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace AuthService.MigrationsJob
+{
+    public sealed class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}
