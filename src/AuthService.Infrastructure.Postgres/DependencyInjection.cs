fa0e2a69e0cfc38b03cf42f435a9fcf04159ccad
Shadrin Maksim <shadrin.maksim.sm@gmail.com>
2025-08-19 22:10:32 +0300

    feat(authservice): настроить регистрацию инфраструктуры Postgres в DI

---
+++ src/AuthService.Infrastructure.Postgres/DependencyInjection.cs
@@ -0,0 +1,61 @@
+using AuthService.Contracts.Options;
+using AuthService.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace AuthService.Infrastructure.Postgres;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddAccountsInfrastructure(this IServiceCollection services)
+    {
+        services.AddOptions<JwtOptions>()
+            .BindConfiguration(JwtOptions.SECTION_NAME);
+
+        services.RegisterIdentity();
+        return services;
+    }
+
+    private static void RegisterIdentity(this IServiceCollection services)
+    {
+        services
+            .AddIdentityCore<User>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddRoles<Role>()
+            .AddEntityFrameworkStores<PostgresDbContext>();
+    }
+
+    public static IServiceCollection AddPostgresInfrastructure(this IServiceCollection services)
+    {
+        services.AddOptions<ConnectionStringsOptions>()
+            .BindConfiguration(ConnectionStringsOptions.SECTION_NAME)
+            .Validate(
+                o => !string.IsNullOrWhiteSpace(o.Database),
+                "Строка подключения к БД (ConnectionStrings:Database) не задана")
+            .ValidateOnStart();
+
+        services.AddDbContextPool<PostgresDbContext>((sp, opt) =>
+        {
+            string cs = sp.GetRequiredService<IOptionsMonitor<ConnectionStringsOptions>>()
+                .CurrentValue.Database;
+
+            opt.UseNpgsql(cs, npg =>
+            {
+                npg.EnableRetryOnFailure();
+                npg.MigrationsHistoryTable("__EFMigrationsHistory", "accounts");
+            });
+
+            opt.UseSnakeCaseNamingConvention();
+
+#if DEBUG
+            opt.EnableDetailedErrors();
+            opt.EnableSensitiveDataLogging();
+#endif
+        });
+
+        return services;
+    }
+}
